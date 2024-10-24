1e2378269a091705456bec670595b5a8b2bdbf6a
forUAi <153248421+forUAi@users.noreply.github.com>
2024-10-24 11:48:00 -0500

    Generate sitemap.xml

---
+++ next-sitemap.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    siteUrl: 'https://diagnoscode-website-jcit.vercel.app',  // Your site's actual URL
+    generateRobotsTxt: true,  // Optional: Generates robots.txt file along with the sitemap
+  };
+  
